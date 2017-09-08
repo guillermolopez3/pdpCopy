7a58e595a5935682ebac6b0d082d38e9cd524237
Guille <guillermolopez3@gmail.com>
2017-09-08 15:40:59 -0300

    sistema que copia los archivos a una carpeta creando carpetas con el mismo nombre. Proyecto funcionando

---
+++ CopiaPdp/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace CopiaPdp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnPathDestino = new System.Windows.Forms.Button();
+            this.btnPathOrigen = new System.Windows.Forms.Button();
+            this.txtPathDestino = new System.Windows.Forms.TextBox();
+            this.txtPathOrigen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvOrigen = new System.Windows.Forms.DataGridView();
+            this.dgvDestino = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RowPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RowFullPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pbProgreso = new System.Windows.Forms.ProgressBar();
+            this.lblTrabajando = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrigen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDestino)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTrabajando);
+            this.groupBox1.Controls.Add(this.pbProgreso);
+            this.groupBox1.Controls.Add(this.btnCopiar);
+            this.groupBox1.Controls.Add(this.btnPathDestino);
+            this.groupBox1.Controls.Add(this.btnPathOrigen);
+            this.groupBox1.Controls.Add(this.txtPathDestino);
+            this.groupBox1.Controls.Add(this.txtPathOrigen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(681, 118);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Origen/Destino";
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(597, 59);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(75, 23);
+            this.btnCopiar.TabIndex = 6;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnPathDestino
+            // 
+            this.btnPathDestino.Location = new System.Drawing.Point(516, 59);
+            this.btnPathDestino.Name = "btnPathDestino";
+            this.btnPathDestino.Size = new System.Drawing.Size(75, 23);
+            this.btnPathDestino.TabIndex = 5;
+            this.btnPathDestino.Text = "Seleccionar";
+            this.btnPathDestino.UseVisualStyleBackColor = true;
+            this.btnPathDestino.Click += new System.EventHandler(this.btnPathDestino_Click);
+            // 
+            // btnPathOrigen
+            // 
+            this.btnPathOrigen.Location = new System.Drawing.Point(516, 23);
+            this.btnPathOrigen.Name = "btnPathOrigen";
+            this.btnPathOrigen.Size = new System.Drawing.Size(75, 23);
+            this.btnPathOrigen.TabIndex = 4;
+            this.btnPathOrigen.Text = "Seleccionar";
+            this.btnPathOrigen.UseVisualStyleBackColor = true;
+            this.btnPathOrigen.Click += new System.EventHandler(this.btnPathOrigen_Click);
+            // 
+            // txtPathDestino
+            // 
+            this.txtPathDestino.Enabled = false;
+            this.txtPathDestino.Location = new System.Drawing.Point(75, 61);
+            this.txtPathDestino.Name = "txtPathDestino";
+            this.txtPathDestino.Size = new System.Drawing.Size(423, 20);
+            this.txtPathDestino.TabIndex = 3;
+            // 
+            // txtPathOrigen
+            // 
+            this.txtPathOrigen.Enabled = false;
+            this.txtPathOrigen.Location = new System.Drawing.Point(75, 25);
+            this.txtPathOrigen.Name = "txtPathOrigen";
+            this.txtPathOrigen.Size = new System.Drawing.Size(423, 20);
+            this.txtPathOrigen.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path Destino";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path Origen";
+            // 
+            // dgvOrigen
+            // 
+            this.dgvOrigen.AllowUserToAddRows = false;
+            this.dgvOrigen.AllowUserToDeleteRows = false;
+            this.dgvOrigen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvOrigen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrigen.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.RowPath,
+            this.RowFullPath});
+            this.dgvOrigen.Location = new System.Drawing.Point(12, 127);
+            this.dgvOrigen.Name = "dgvOrigen";
+            this.dgvOrigen.ReadOnly = true;
+            this.dgvOrigen.RowHeadersVisible = false;
+            this.dgvOrigen.Size = new System.Drawing.Size(324, 393);
+            this.dgvOrigen.TabIndex = 1;
+            // 
+            // dgvDestino
+            // 
+            this.dgvDestino.AllowUserToAddRows = false;
+            this.dgvDestino.AllowUserToDeleteRows = false;
+            this.dgvDestino.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDestino.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDestino.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.dgvDestino.Location = new System.Drawing.Point(353, 127);
+            this.dgvDestino.Name = "dgvDestino";
+            this.dgvDestino.ReadOnly = true;
+            this.dgvDestino.RowHeadersVisible = false;
+            this.dgvDestino.Size = new System.Drawing.Size(331, 393);
+            this.dgvDestino.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Carpetas";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // RowPath
+            // 
+            this.RowPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.RowPath.HeaderText = "Archivos PDP";
+            this.RowPath.Name = "RowPath";
+            this.RowPath.ReadOnly = true;
+            // 
+            // RowFullPath
+            // 
+            this.RowFullPath.HeaderText = "Column1";
+            this.RowFullPath.Name = "RowFullPath";
+            this.RowFullPath.ReadOnly = true;
+            this.RowFullPath.Visible = false;
+            // 
+            // pbProgreso
+            // 
+            this.pbProgreso.Location = new System.Drawing.Point(75, 87);
+            this.pbProgreso.Name = "pbProgreso";
+            this.pbProgreso.Size = new System.Drawing.Size(151, 23);
+            this.pbProgreso.TabIndex = 7;
+            // 
+            // lblTrabajando
+            // 
+            this.lblTrabajando.AutoSize = true;
+            this.lblTrabajando.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTrabajando.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblTrabajando.Location = new System.Drawing.Point(232, 87);
+            this.lblTrabajando.Name = "lblTrabajando";
+            this.lblTrabajando.Size = new System.Drawing.Size(15, 18);
+            this.lblTrabajando.TabIndex = 8;
+            this.lblTrabajando.Text = "*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 532);
+            this.Controls.Add(this.dgvDestino);
+            this.Controls.Add(this.dgvOrigen);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PDP Copy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrigen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDestino)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnPathDestino;
+        private System.Windows.Forms.Button btnPathOrigen;
+        private System.Windows.Forms.TextBox txtPathDestino;
+        private System.Windows.Forms.TextBox txtPathOrigen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvOrigen;
+        private System.Windows.Forms.DataGridView dgvDestino;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RowPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RowFullPath;
+        private System.Windows.Forms.ProgressBar pbProgreso;
+        private System.Windows.Forms.Label lblTrabajando;
+    }
+}
+
---
+++ CopiaPdp/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CopiaPdp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPathOrigen_Click(object sender, EventArgs e)
+        {
+            obtenerPathOrigen();
+        }
+
+        private void btnPathDestino_Click(object sender, EventArgs e)
+        {
+            obtenerPathDestino();
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            if ((string.IsNullOrEmpty(txtPathOrigen.Text)) || (string.IsNullOrEmpty(txtPathDestino.Text)))
+            {
+                MessageBox.Show("No pueden quedar vacios los Path Origen ni Destino");
+                return;
+            }
+            crearCarpetasFaltantes(); //verifico si falta alguna carpeta en el destino, la creo
+
+            
+            copiar();
+        }
+
+        private void obtenerPathOrigen() //obtengo los archivos .rar con los pdp
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+        
+            if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                dgvOrigen.Rows.Clear();
+                txtPathOrigen.Text = dialog.SelectedPath;
+                DirectoryInfo source = new DirectoryInfo(dialog.SelectedPath);
+
+                FileInfo[] archivos = source.GetFiles();
+               
+
+                foreach(FileInfo info in archivos)
+                {
+                    
+                    dgvOrigen.Rows.Add(Path.GetFileNameWithoutExtension(info.ToString()),Path.GetExtension(info.ToString())); //agrego el nombre sin la extencion
+                }
+               
+
+            }
+                       
+        }
+
+        private void obtenerPathDestino() //muestro las carpetas donde se van a copiar
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                dgvDestino.Rows.Clear();
+                txtPathDestino.Text = dialog.SelectedPath;
+                DirectoryInfo source = new DirectoryInfo(dialog.SelectedPath);
+
+                DirectoryInfo[] carpetas = source.GetDirectories();
+
+                foreach (DirectoryInfo di in carpetas)
+                {
+                    dgvDestino.Rows.Add(di.ToString());
+                }
+
+            }
+        }
+
+
+        private void crearCarpetasFaltantes() //me fijo el nombre de los archivos y creo las carpetas faltantes
+        {
+            string path_o = @txtPathOrigen.Text;
+            string path_d = @txtPathDestino.Text;
+            int contador = 0; //variable para ver ctas carpetas voy a crear
+
+            List<String> lista = new List<string>();
+
+            for (int i = 0; i < dgvOrigen.RowCount; i++) //recorro la grilla origen
+            {
+                string fila_origen = dgvOrigen.Rows[i].Cells[0].Value.ToString();
+                bool existe_carpeta = false; //variable para ver si la carpeta existe en el destino
+
+                for (int j = 0; j < dgvDestino.RowCount; j++) //recorro la grilla destino
+                {
+                    string fila_destino = dgvDestino.Rows[j].Cells[0].Value.ToString();
+
+                    if (string.Equals(fila_origen, fila_destino)) //si alguna fila de la grilla origen es igual a la del detino, avanzo
+                    {
+                        existe_carpeta = true;
+                        break;
+                    }
+                }
+
+                if(existe_carpeta==false) //si al recorrer toda la grilla de la carpeta destino no encontro una carpeta con el nombre del archivo
+                {
+                    lista.Add(fila_origen);
+                    contador++;
+                }
+            }
+
+            if(contador==0)
+            {
+                return;
+            }
+
+            if (MessageBox.Show("Se van a crear " + contador + " nuevas carpetas en destino. ¿Confirmar?", "Craer carpetas", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                string nombre_carpeta_destino = "";
+
+                foreach (string li in lista)
+                {
+                    nombre_carpeta_destino = Path.Combine(path_d, li);
+
+                    if (!Directory.Exists(nombre_carpeta_destino)) //veo si existe la carpeta, si no la creo
+                    {
+                        Directory.CreateDirectory(nombre_carpeta_destino);
+                    }
+                }
+
+                dgvDestino.Rows.Clear();
+
+                DirectoryInfo source = new DirectoryInfo(@txtPathDestino.Text);
+
+                DirectoryInfo[] carpetas = source.GetDirectories();
+
+                foreach (DirectoryInfo di in carpetas)
+                {
+                    dgvDestino.Rows.Add(di.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Carpetas no creadas");
+                return;
+            }
+        }
+
+
+        private void copiar() //preparo la ruta origen y la destino
+        {
+            string full_path_origen = "";
+            string full_path_destino = "";
+            DateTime date = DateTime.Now;
+            string fecha = date.ToShortDateString();
+            string fecha_final = fecha.Replace("/", "");
+
+            pbProgreso.Maximum = dgvOrigen.RowCount;
+            progreso("Iniiciando", 0);
+
+
+            for (int i=0; i<dgvOrigen.RowCount;i++)
+            {
+                full_path_origen = @txtPathOrigen.Text + "\\"+ dgvOrigen.Rows[i].Cells[0].Value.ToString() + dgvOrigen.Rows[i].Cells[1].Value.ToString();
+
+                for(int j=0; j<dgvDestino.RowCount;j++)
+                {
+                    //full_path_destino = lo que hay en el txt destino + la carpeta destino + nombre origen + fecha + extencion
+                    full_path_destino = @txtPathDestino.Text + "\\" + dgvDestino.Rows[j].Cells[0].Value.ToString() + "\\" + dgvOrigen.Rows[i].Cells[0].Value.ToString() + "_" + fecha_final + dgvOrigen.Rows[i].Cells[1].Value.ToString();
+                    if (string.Equals(dgvOrigen.Rows[i].Cells[0].Value.ToString(),dgvDestino.Rows[j].Cells[0].Value.ToString() )) //si el archivo es igual a la carpeta, copio el archivo
+                    {
+                        if(File.Exists(full_path_destino))
+                        {
+                            string nuevo_nom = nuevoNombre(@txtPathDestino.Text + "\\" + dgvDestino.Rows[j].Cells[0].Value.ToString(),dgvOrigen.Rows[i].Cells[0].Value.ToString() + "_" + fecha_final);
+                            string nuevo_pa = @txtPathDestino.Text + "\\" + dgvDestino.Rows[j].Cells[0].Value.ToString() + "\\" + nuevo_nom + dgvOrigen.Rows[i].Cells[1].Value.ToString();
+
+                            try
+                            {
+                                File.Copy(full_path_origen, nuevo_pa, false);
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show("Error al copiar el archivo " + full_path_origen + ". Todos los archivos siguientes a éste no se van a copiar", "Error");
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                File.Copy(full_path_origen, full_path_destino, false);
+                            }
+                            catch (Exception e)
+                            {
+                                MessageBox.Show("Error al copiar el archivo " + full_path_origen + ". Todos los archivos siguientes a éste no se van a copiar", "Error");
+                                return;
+                            }
+                            
+                        }
+                        break;
+                    }
+                }
+                progreso(string.Format("posicion {0}...", i), i);
+            }
+            progreso("Fin", dgvOrigen.RowCount);
+            MessageBox.Show("Todos los archivos fueron copiados","Fin copiado");
+        }
+
+        private string nuevoNombre(string directorio, string nombre_archivo)
+        {
+            string nuevo_nombre = nombre_archivo;
+            int con = 0;
+           
+            DirectoryInfo source = new DirectoryInfo(directorio);
+
+            FileInfo[] archivos = source.GetFiles();
+
+
+            for(int i=0;i<archivos.Length;i++)
+            {
+                if(string.Equals(nuevo_nombre, Path.GetFileNameWithoutExtension(archivos[i].ToString())))
+                {
+                    i = -1;
+                    con++;
+                    nuevo_nombre = nombre_archivo + "("+con + ")";
+                }
+            }
+         
+
+            return nuevo_nombre;
+        }
+
+
+        delegate void CambiarProgresoDelegado(string texto, int valor);
+
+        private void progreso(string texto, int valor)
+        {
+            if (this.InvokeRequired) //preguntamos si la llamada se hace desde un hilo 
+            {
+                //si es así entonces volvemos a llamar a CambiarProgreso pero esta vez a través del delegado 
+                //instanciamos el delegado indicandole el método que va a ejecutar 
+                CambiarProgresoDelegado delegado = new CambiarProgresoDelegado(progreso);
+                //ya que el delegado invocará a CambiarProgreso debemos indicarle los parámetros 
+                object[] parametros = new object[] { texto, valor };
+                //invocamos el método a través del mismo contexto del formulario (this) y enviamos los parámetros 
+                this.Invoke(delegado, parametros);
+            }
+            else
+            {
+                //en caso contrario, se realiza el llamado a los controles 
+                lblTrabajando.Text = texto;
+                pbProgreso.Value = valor;
+            }
+
+        }
+
+    }
+}
